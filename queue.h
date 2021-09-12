6dc8b567acbf9777b07968804e6d53d7d67eb404
Gary <ricardo.valer@utec.edu.pe.com>
2021-09-12 02:50:44 -0500

    raaa3

--- queue.h
+++ queue.h
@@ -6,5 +6,81 @@ class QueueArray : public CircularArray<T> {
         int front,back;
         int capacity; 
     public:
-        
+        QueueArray(int _capacity);
+        ~QueueArray();
+        void enqueue(T data);
+        T dequeue();
+        bool is_full();
+        bool is_empty();
+    private:
+        int next(int index);
+        int prev(int index);
 };
+
+template <class T>
+QueueArray<T>::QueueArray(int _capacity){
+    this->queue = new int[_capacity];
+    this->capacity = _capacity;
+    this->front = this->back = -1;
+}
+
+template <class T>
+QueueArray<T>::~QueueArray(){
+    delete[] queue;
+}
+
+template<class T>
+int QueueArray<T>::next(int index){
+    return (index + 1);
+}
+
+template<class T>
+int QueueArray<T>::prev(int index){
+    return index - 1;
+}
+
+template <class T>
+bool QueueArray<T>::is_full(){
+    return (front == 0 && back == capacity - 1) || (back == front - 1);
+}
+
+template <class T>
+bool QueueArray<T>::is_empty(){
+    return front == -1;
+}
+
+template <class T>
+void QueueArray<T>::enqueue(T data){
+    if(is_full()){
+        throw "Array is full";
+    }
+    if(is_empty){
+        front = back = 0;
+    }
+    else if(back == capacity - 1){
+        back = 0;
+    }
+    else{
+        back++;
+    }
+     array[back] = data;
+}
+
+template <class T>
+T QueueArray<T>::dequeue(){
+    if(is_empty()){
+        throw "Error";
+    }
+    T data = queue[front];
+    if(front == back){
+        front = back = -1;
+    }
+    else if(back == capacity - 1){
+        front = 0;
+    }
+    else{
+        front++;
+    }
+    return data;
+}
+
--- stack.h
+++ stack.h
@@ -2,5 +2,69 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> { 
-    
+        T* stack;
+        int capacity;
+        int size;
+    public:
+        StackArray(int _capacity);
+        ~StackArray();
+        void push(T data);
+        T pop();
+        void resize(int new_size);
+    private:
+        int next(int index);
+        int prev(int index);
 };
+
+template <class T>
+StackArray<T>::StackArray(int _capacity){
+    this->capacity = _capacity;
+    this->size = 0;
+    this->stack = new T[_capacity];
+}
+
+template <class T>
+StackArray<T>::~StackArray()
+{
+   delete[] stack;
+}
+
+template<class T>
+int StackArray<T>::next(int index){
+    return (index + 1);
+}
+
+template<class T>
+int StackArray<T>::prev(int index){
+    return index - 1;
+}
+
+template <class T>
+void StackArray<T>::push(T data){
+    this->stack[this->size++] = data; 
+    if (this->size == this->capacity){
+        resize(this->capacity*2);
+    }
+}
+
+template <class T>
+T StackArray<T>::pop(){
+    if(this->size > 0){
+        return array[--this->size]; 
+    }
+}
+
+template<class T>
+void StackArray<T>::resize(int new_size){
+    this->capacity = new_size;
+    int *temp = new int[this->capacity];
+    for (int i = 0; i < size; i++)
+        temp[i] = stack[i];
+    delete[] stack;
+    stack = temp;
+}
+
+
+
+
+
