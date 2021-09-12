9184490370086a6ff953cf76cf7048202c02a562
Gary <ricardo.valer@utec.edu.pe.com>
2021-09-12 01:22:35 -0500

    raaa2

--- queue.h
+++ queue.h
@@ -1,6 +1,10 @@
 #include "circulararray.h"
 
 template <typename T>
-class QueueArray : public CircularArray<T> { 
-    
+class QueueArray : public CircularArray<T> {
+        T *queue; 
+        int front,back;
+        int capacity; 
+    public:
+        
 };
